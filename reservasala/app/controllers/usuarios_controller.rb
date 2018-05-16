f2d828e59169cd8dd2ebb8a7e2fdec18f6871c8f
lobao5670 <lobao5670@gmail.com>
2018-05-16 15:02:03 -0300

    cadastro de usuario completo

--- reservasala/app/controllers/usuarios_controller.rb
+++ reservasala/app/controllers/usuarios_controller.rb
@@ -8,6 +8,34 @@ class UsuariosController < ApplicationController
   end
 
   def create
+    @usuario = Usuario.new(usuario_params)
+    respond_to do |format|
+      @errorNome = []
+      @errorEmail = []
+      @errorSenha = []
+      if @usuario.save
+        format.js
+        format.json
+        flash[:success] = "Usuario cadastrado com sucesso!"
+        redirect_to usuarios_path
+      else
+        format.json
+        format.js
+        @usuario.errors.any?
+        if (@usuario.errors["nome"] != nil)
+          @errorNome.push(@usuario.errors["nome"][0])
+        end
+        if (@usuario.errors["email"] != nil)
+          @errorEmail.push(@usuario.errors["email"][0])
+        end
+        if (@usuario.errors["senha"] != nil)
+          @errorSenha.push(@usuario.errors["senha"][0])
+        end
+      end
+    end
+  end
 
+  def usuario_params
+    params.require(:usuario).permit(:nome, :email, :senha)
   end
 end
--- reservasala/app/models/usuario.rb
+++ reservasala/app/models/usuario.rb
@@ -1,8 +1,9 @@
 class Usuario < ActiveRecord::Base
-  validates :nome, presence: true
+  validates :nome, presence: {message: "Informe um nome"}
 
   before_save { self.email = email.downcase }
-  validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}, uniqueness: {case_sensitive: false}
+  validates :email, presence: {message: "Informe um email"}, uniqueness: {case_sensitive: false, message: "Email já cadastrado"}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, message: "Informe um email válido"
 
-  validates :senha, presence: true
+  validates :senha, presence: {message: "Informe uma senha"}
 end
