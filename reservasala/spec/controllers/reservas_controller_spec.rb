94c2ad0c3959d190bcee1b33fe0952105882f42b
lobao5670 <lobao5670@gmail.com>
2018-07-02 17:36:54 -0300

    corrigindo argumento no get rspec

--- reservasala/spec/controllers/reservas_controller_spec.rb
+++ reservasala/spec/controllers/reservas_controller_spec.rb
@@ -66,22 +66,22 @@ describe ReservasController do
       request.session[:current_user_id] = 2
     end
     it 'should render the right template for reservas_sala' do
-      get :reservas_sala, {:id => '1'}
+      get :reservas_sala, :params => {:id => '1'}
       expect(response).to render_template('reservas_sala')
     end
     it 'should initialize the right room' do
       sala = Sala.find_by_id 1
-      get :reservas_sala, {:id => '1'}
+      get :reservas_sala, :params => {:id => '1'}
       expect(assigns(:sala)).to eq(sala)
     end
     it 'should get the correct reservations for that room' do
       reservas_s = Reserva.where(sala_id: 1)
-      get :reservas_sala, {:id => '1'}
+      get :reservas_sala, :params => {:id => '1'}
       expect(assigns(:reservas_s)).to match_array(reservas_s)
     end
     it 'should not get the reservation from another room' do
       reserva = Reserva.where(sala_id: 2).first
-      get :reservas_sala, {:id => '1'}
+      get :reservas_sala, :params => {:id => '1'}
       expect(assigns(:reservas_s)).to_not include(reserva)
     end
   end
